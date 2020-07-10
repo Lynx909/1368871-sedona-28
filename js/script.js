5ba01b3ea53b6428868a4ffd5b3cc27453734ca7
Lynx909 <novikova-annya@mail.ru>
2020-07-10 09:23:37 +0300

    JS 2

--- js/script.js
+++
@@ -1,6 +0,0 @@
-var IndexButtonFormClose = document.querySelector(".index-button-form");
-var ItemIndexForm = document.querySelector(".item-index-form");
-IndexButtonFormClose.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    ItemIndexForm.classList.add("item-index-form-show");
-});
